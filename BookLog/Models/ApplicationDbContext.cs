061657743af7edd9e2de3d82c823c629a92e471e
Dusan <dusan.kovarik@outlook.com>
2025-12-15 21:59:55 +0100

    Add Review model class and add Reviews DbSet

--- BookLog/Models/ApplicationDbContext.cs
+++ BookLog/Models/ApplicationDbContext.cs
@@ -7,5 +7,6 @@ namespace BookLog.Models {
         public DbSet<Author> Authors { get; set; }
         public DbSet<Genre> Genres { get; set; }
         public DbSet<Book> Books { get; set; }
+        public DbSet<Review> Reviews { get; set; }
     }
 }
---
+++ BookLog/Models/Review.cs
@@ -0,0 +1,10 @@
+﻿namespace BookLog.Models {
+    public class Review {
+        public int Id { get; set; }
+        public int BookId { get; set; }
+        public required Book Book { get; set; }
+        public int Rating { get; set; }
+        public required string Text { get; set; }
+        public DateOnly CreatedAt { get; set; }
+    }
+}
