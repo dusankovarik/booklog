f86ade796a3bc6803982bf4fea25e33a97c8ee9e
Dusan <dusan.kovarik@outlook.com>
2025-12-17 21:19:39 +0100

    Add roles feature

---
+++ BookLog/Controllers/RolesController.cs
@@ -0,0 +1,102 @@
+﻿using BookLog.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookLog.Controllers {
+    public class RolesController : Controller {
+        private RoleManager<IdentityRole> _roleManager;
+        private UserManager<AppUser> _userManager; 
+
+        public RolesController(RoleManager<IdentityRole> roleManager, UserManager<AppUser> userManager) {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index() {
+            return View(_roleManager.Roles);
+        }
+
+        public IActionResult Create() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync(string name) {
+            if (ModelState.IsValid) {
+                IdentityResult result = await _roleManager.CreateAsync(new IdentityRole(name));
+                if (result.Succeeded) {
+                    return RedirectToAction("Index");
+                }
+                AddErrors(result);
+            }
+            return View("Create", name);
+        }
+
+        public async Task<IActionResult> EditAsync(string id) {
+            var role = await _roleManager.FindByIdAsync(id);
+            if (role == null) {
+                return View("NotFound");
+            }
+            var members = new List<AppUser>();
+            var nonMembers = new List<AppUser>();
+            foreach (AppUser user in _userManager.Users) {
+                var list = await _userManager.IsInRoleAsync(user, role.Name!) ? members : nonMembers;
+                list.Add(user);
+            }
+            return View(new RoleEdit {
+                Role = role,
+                Members = members,
+                NonMembers = nonMembers,
+            });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditAsync(RoleModification model) {
+            IdentityResult result;
+            if (ModelState.IsValid) {
+                foreach (string userId in model.IdsToAdd ?? []) {
+                    var user = await _userManager.FindByIdAsync(userId);
+                    if (user != null) {
+                        result = await _userManager.AddToRoleAsync(user, model.RoleName);
+                        if (!result.Succeeded) {
+                            AddErrors(result);
+                        }
+                    }
+                }
+                foreach (string userId in model.IdsToDelete ?? []) {
+                    var user = await _userManager.FindByIdAsync(userId);
+                    if (user != null) {
+                        result = await _userManager.RemoveFromRoleAsync(user, model.RoleName);
+                        if (!result.Succeeded) {
+                            AddErrors(result);
+                        }
+                    }
+                }
+                if (ModelState.IsValid) {
+                    return RedirectToAction("Index");
+                }
+            }
+            return await EditAsync(model.RoleId);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteAsync(string id) {
+            var role = await _roleManager.FindByIdAsync(id);
+            if (role != null) {
+                IdentityResult result = await _roleManager.DeleteAsync(role);
+                if (result.Succeeded) {
+                    return RedirectToAction("Index");
+                }
+                AddErrors(result);
+            }
+            else {
+                ModelState.AddModelError("", "Role not found.");
+            }
+            return View("Index", _roleManager.Roles);
+        }
+
+        private void AddErrors(IdentityResult result) {
+            foreach (IdentityError error in result.Errors) {
+                ModelState.AddModelError("", error.Description);
+            }
+        }
+    }
+}
---
+++ BookLog/Models/RoleEdit.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BookLog.Models {
+    public class RoleEdit {
+        public required IdentityRole Role { get; set; }
+        public required IEnumerable<AppUser> Members { get; set; }
+        public required IEnumerable<AppUser> NonMembers { get; set; }
+
+    }
+}
